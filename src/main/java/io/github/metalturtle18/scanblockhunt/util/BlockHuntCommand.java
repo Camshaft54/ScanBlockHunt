64edbba27d116a05e0a59187d27fb9949049f87f
BuildTools <unconfigured@null.spigotmc.org>
2021-05-06 10:51:17 -0700

    Add javadoc comments for Game.java

--- src/main/java/io/github/metalturtle18/scanblockhunt/util/BlockHuntCommand.java
+++ src/main/java/io/github/metalturtle18/scanblockhunt/util/BlockHuntCommand.java
@@ -3,7 +3,15 @@ package io.github.metalturtle18.scanblockhunt.util;
 import org.bukkit.entity.Player;
 
 public interface BlockHuntCommand {
+    /**
+     * @return the name of the command to be used by the CommandManager
+     */
     String getCommandName();
 
+    /**
+     * The method executed when a player runs it.
+     * @param player the player who ran the command
+     * @param args the args of the command
+     */
     void runCommand(Player player, String[] args);
 }
--- src/main/java/io/github/metalturtle18/scanblockhunt/util/Game.java
+++ src/main/java/io/github/metalturtle18/scanblockhunt/util/Game.java
@@ -10,33 +10,51 @@ import org.bukkit.scoreboard.*;
 
 import java.util.HashMap;
 
+/**
+ * The Game class stores data about the current game, including the players, current item, and elapsed time for the
+ * current round. It also contains methods for modifying the aforementioned values.
+ * @author Camshaft54 and MetalTurtle18
+ */
 public class Game {
-    @Getter private final Player gameHost;
-    @Getter private final HashMap<Player, Score> players = new HashMap<>();
-    @Getter private Material currentItem;
+    @Getter
+    private final Player gameHost;
+    @Getter
+    private final HashMap<Player, Score> players = new HashMap<>();
+    @Getter
+    private Material currentItem;
     private int roundNumber;
     private int roundTime;
     private Team roundTimeText;
     private Scoreboard scoreboard;
     private Objective itemObj;
 
+    /**
+     * Initializes the Game class and displays the scoreboard to all players currently in the game.
+     * @param host the player who is hosting the game
+     */
     public Game(Player host) {
         currentItem = null;
         roundNumber = 0;
-        roundTime = -1;
+        roundTime = 0;
         if (Bukkit.getScoreboardManager() != null) {
             scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
             itemObj = scoreboard.registerNewObjective("item", "", "N/A Round 0");
             roundTimeText = scoreboard.registerNewTeam("- secs");
             itemObj.getScore(roundTimeText.getDisplayName()).setScore(999);
             itemObj.setDisplaySlot(DisplaySlot.SIDEBAR);
-            updateScoreboardForAllPlayers();
+            for (Player p : players.keySet()) {
+                p.setScoreboard(scoreboard);
+            }
         }
         // TODO: Disable plugin if manager is null
         gameHost = host;
         addPlayer(host);
     }
 
+    /**
+     * Adds a player to the game with a score of 0 and displays the game's scoreboard to them.
+     * @param player the player to be added to the game
+     */
     public void addPlayer(Player player) {
         scoreboard.registerNewTeam(player.getDisplayName());
         players.put(player, itemObj.getScore(player.getDisplayName()));
@@ -44,26 +62,46 @@ public class Game {
         player.setScoreboard(scoreboard);
     }
 
+    /**
+     * Removes a player from the game and hides the game scoreboard from them.
+     * @param player the player to be removed from the game
+     */
     public void removePlayer(Player player) {
         Team playerTeam = scoreboard.getTeam(player.getDisplayName());
-        if (playerTeam != null) {
+        if (playerTeam != null && Bukkit.getScoreboardManager() != null) {
             playerTeam.unregister();
+            Scoreboard dummyScoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+            Objective dummyObj = dummyScoreboard.registerNewObjective("dummy", "", "");
+            dummyObj.setDisplaySlot(DisplaySlot.SIDEBAR);
+            player.setScoreboard(dummyScoreboard);
             players.remove(player);
         }
     }
 
+    /**
+     * Resets the scores for all players in the game.
+     */
     public void resetScores() {
         for (Player p : players.keySet()) {
             players.get(p).setScore(0);
         }
     }
 
-    public void adjustScore(Player player, int amount) {
+    /**
+     * Increases the score of a player by a specified amount.
+     * @param player the player to have their score modified
+     * @param amount the number of points to add or, if negative, subtract from the player
+     */
+    public void increaseScore(Player player, int amount) {
         if (players.containsKey(player)) {
             players.get(player).setScore(players.get(player).getScore() + amount);
         }
     }
 
+    /**
+     * Sets the next rounds item and starts the new round.
+     * @param item the material that players will search for in the next round
+     */
     public void setItem(Material item) {
         currentItem = item;
         ScanBlockHunt.roundGoing = true;
@@ -74,46 +112,51 @@ public class Game {
         roundTime = 0;
     }
 
+    /**
+     * Ends the current round and adds a point to the specified player's score.
+     * @param foundByPlayer the player who found the item and will be rewarded a point
+     */
     public void itemFound(Player foundByPlayer) {
         ScanBlockHunt.roundGoing = false;
-        Messenger.sendTitle( ChatColor.RED + foundByPlayer.getDisplayName() + " found " + currentItem + "!", "");
+        Messenger.sendTitle(ChatColor.RED + foundByPlayer.getDisplayName() + " found " + currentItem + "!", "");
         players.get(foundByPlayer).setScore(players.get(foundByPlayer).getScore() + 1);
     }
 
+    /**
+     * Ends the current round without declaring a winner.
+     */
     public void endRound() {
         ScanBlockHunt.roundGoing = false;
-        Messenger.sendTitle("Round skipped!", "No one found " + currentItem);
         roundTime = 0;
+        Messenger.sendTitle("Round skipped!", "No one found " + currentItem);
     }
 
+    /**
+     * Ends the entire game and removes all the players from it.
+     */
     public void endGame() {
         ScanBlockHunt.roundGoing = false;
         if (Bukkit.getScoreboardManager() != null) {
-            Scoreboard dummyScoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-            Objective dummyObj = dummyScoreboard.registerNewObjective("dummy", "", "");
-            dummyObj.setDisplaySlot(DisplaySlot.SIDEBAR);
             for (Player p : players.keySet()) {
-                p.setScoreboard(dummyScoreboard);
+                removePlayer(p);
             }
         }
     }
 
+    /**
+     * Increase the time that has elapsed in the round by 1 second. This method also handles the string being too long
+     * to display on the leaderboard.
+     */
     public void increaseTimeElapsed() {
         if (ScanBlockHunt.roundGoing) {
             scoreboard.resetScores(roundTimeText.getDisplayName());
             roundTime++;
-            if (("" + roundTime).length() + 5 > 15) {
-                roundTimeText.setDisplayName(">1000000000 secs");
+            if (("" + roundTime).length() > 7) {
+                roundTimeText.setDisplayName(">1000000 seconds");
             } else {
-                roundTimeText.setDisplayName(roundTime + " secs");
+                roundTimeText.setDisplayName(roundTime + " seconds");
             }
             itemObj.getScore(roundTimeText.getDisplayName()).setScore(999);
-        }
-    }
-
-    public void updateScoreboardForAllPlayers() {
-        for (Player p : players.keySet()) {
-            p.setScoreboard(scoreboard);
         }
     }
 }
